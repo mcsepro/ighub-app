f0873fa5384b19a63a0b770e34e321c3f1602300
mcsepro <aremsony2k4@yahoo.com>
2020-08-06 15:31:54 +0100

    my changes

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-         return view('home.index');
-    }
+    public function layout()
+   {
+         return view('home.layout');
+   }
 }
+
+
